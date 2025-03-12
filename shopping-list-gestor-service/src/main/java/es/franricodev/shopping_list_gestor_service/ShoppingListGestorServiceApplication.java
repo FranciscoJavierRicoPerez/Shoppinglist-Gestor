98182d7cc92abf96bbd366261d2f2774be1a9e22
DESKTOP-4G5I4SA\Usuario <franricoperez1997@gmail.com>
2025-03-12 12:59:31 +0100

    Añadido proyecto Spring Boot

---
+++ shopping-list-gestor-service/src/main/java/es/franricodev/shopping_list_gestor_service/ShoppingListGestorServiceApplication.java
@@ -0,0 +1,13 @@
+package es.franricodev.shopping_list_gestor_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingListGestorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingListGestorServiceApplication.class, args);
+	}
+
+}
---
+++ shopping-list-gestor-service/src/test/java/es/franricodev/shopping_list_gestor_service/ShoppingListGestorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package es.franricodev.shopping_list_gestor_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingListGestorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
