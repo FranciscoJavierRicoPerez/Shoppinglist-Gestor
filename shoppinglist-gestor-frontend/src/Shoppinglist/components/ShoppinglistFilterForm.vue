6321b1de965ce4a8c5369d761f5f87c8a46fef92
DESKTOP-4G5I4SA\Usuario <franricoperez1997@gmail.com>
2025-03-31 17:33:49 +0200

    Eliminacion de archivo innecesario

--- shoppinglist-gestor-frontend/src/Shoppinglist/components/ShoppinglistFilterForm.vue
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-import DatePicker from 'primevue/datepicker'
-import { ref } from 'vue'
-
-const date = ref()
-</script>
-<template>
-  <DatePicker v-model="date"></DatePicker>
-</template>
