b84957374015d3d7bbd44383a98dcbef344024b4
DESKTOP-4G5I4SA\Usuario <franricoperez1997@gmail.com>
2025-04-24 09:49:07 +0200

    Creado fichero .env para manejar entornos

--- shoppinglist-gestor-mobile/src/Shoppinglist/infrastructure/useCases/getAllShoppinglist.ts
+++ shoppinglist-gestor-mobile/src/Shoppinglist/infrastructure/useCases/getAllShoppinglist.ts
@@ -2,8 +2,16 @@ import type { Shoppinglist } from '@/Shoppinglist/domain/Shoppinglist'
 import type { ResponseShoppinglist } from '../models/ResponseShoppinglist'
 import responseGetShoppinglist from '@/Shoppinglist/infrastructure/mocks/responseGetAllShoppinglist.json'
 import { createShoppinglistArray } from '@/Shoppinglist/infrastructure/services/ShoppinglistService'
+
 async function getAllShoppinglist(): Promise<Shoppinglist[]> {
-  return createShoppinglistArray(await InMemory())
+  console.log(import.meta.env.MODE)
+  const response : ResponseShoppinglist[] = import.meta.env.VITE_DATA_ACCESS === 'LOCAL' ? await InMemory() : await Api();
+  return createShoppinglistArray(response)
+}
+
+async function Api(): Promise<ResponseShoppinglist[]>{
+  console.log('API');
+  return [];
 }
 
 async function InMemory(): Promise<ResponseShoppinglist[]> {
