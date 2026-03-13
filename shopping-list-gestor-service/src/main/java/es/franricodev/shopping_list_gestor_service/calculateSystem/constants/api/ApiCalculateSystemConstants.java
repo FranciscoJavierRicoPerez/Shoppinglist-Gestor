99da817caf2640e1c44ef0c79e798de36350b7e9
DESKTOP-4G5I4SA\Usuario <franricoperez1997@gmail.com>
2026-03-13 19:25:53 +0100

    Actualizado API de Calculate System

---
+++ shopping-list-gestor-service/src/main/java/es/franricodev/shopping_list_gestor_service/calculateSystem/constants/api/ApiCalculateSystemConstants.java
@@ -0,0 +1,10 @@
+package es.franricodev.shopping_list_gestor_service.calculateSystem.constants;
+
+public class ApiCalculateSystemConstants {
+    // -------------- GENERAL CONSTANSTS ---------------------
+    public static final String API_NAME = "Calculate System Controller API Documentation";
+    public static final String API_DESCRIPTION = "Management of the operations related with the calculate system";
+    // -------------- URL CONSTANTS --------------------------
+    public static final String BASE_URL = "/api/calculatesystem";
+    public static final String GET_ALL_V1 = "/v1/all";
+}
