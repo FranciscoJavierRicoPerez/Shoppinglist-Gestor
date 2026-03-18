4737db8e808e4bff59b14ca67cbcf8615edee61a
DESKTOP-4G5I4SA\Usuario <franricoperez1997@gmail.com>
2026-03-18 16:48:01 +0100

    Refactoizarion de la interfaz de Api de producto

---
+++ shopping-list-gestor-service/src/main/java/es/franricodev/shopping_list_gestor_service/product/constants/api/ApiProductConstants.java
@@ -0,0 +1,14 @@
+package es.franricodev.shopping_list_gestor_service.product.constants.api;
+
+public class ApiProductConstants {
+    // -------------------- GENERAL CONSTANTS ----------------------
+    public static final String API_NAME = "Product Controller API Documentation";
+    public static final String API_DESCRIPTION = "Management of the operations related with the products";
+    // -------------------- URL CONSTANTS --------------------------
+    public static final String BASE_URL = "/api/product";
+    public static final String GET_ALL_PRODUCTS_V1 = "/v1/all";
+    public static final String GET_ALL_PRODUCTS_NAMES_V1 = "/v1/names";
+    // -------------------- OPERATIONS DESCRIPTIONS ----------------
+    public static final String GET_ALL_PRODUCTS_V1_OP_SUMMARY = "Returns all the products in the database";
+    public static final String GET_ALL_PRODUCTS_NAMES_V1_OP_SUMMARY = "Return the name of all products in the database";
+}
