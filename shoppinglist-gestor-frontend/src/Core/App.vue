ad86fa1fc74bc87eed560f325886c60382204857
DESKTOP-4G5I4SA\Usuario <franricoperez1997@gmail.com>
2026-09-25 09:24:23 +0200

    añadidos valores de configuracion

--- shoppinglist-gestor-frontend/src/Core/App.vue
+++ shoppinglist-gestor-frontend/src/Core/App.vue
@@ -1,10 +1,21 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import Navbar from '@/Shared/components/Navbar.vue'
-import { onErrorCaptured } from 'vue'
+import { computed, onErrorCaptured } from 'vue'
 import { useToast } from 'primevue'
 
 const toast = useToast()
+
+const actualAppVersion = computed(() => {
+  return import.meta.env.ACTUAL_APP_VERSION
+})
+const copyright = computed(() => {
+  return 'GestCompras - Francisco Javier Rico Pérez'
+})
+
+const appName = computed(() => {
+  return 'GestCompras'
+})
 
 onErrorCaptured((error) => {
   console.log(error.message)
@@ -20,7 +31,7 @@ onErrorCaptured((error) => {
 <template>
   <div class="flex flex-column justify-content-between">
     <div class="bg-green-400 w-full p-3">
-      <div class="flex text-6xl font-italic justify-content-center">GestCompras</div>
+      <div class="flex text-6xl font-italic justify-content-center">{{ appName }}</div>
     </div>
     <header class="p-3">
       <Navbar></Navbar>
@@ -31,7 +42,7 @@ onErrorCaptured((error) => {
 
     <footer class="text-3xl bg-green-400 mt-9 w-full p-3">
       <div class="flex text-3xl font-italic justify-content-center">
-        Francisco Javier Rico Pérez - 2026 v0.2.0-SNAPSHOT
+        {{ copyright }}{{ actualAppVersion }}
       </div>
     </footer>
   </div>
